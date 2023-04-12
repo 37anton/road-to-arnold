9988b14092a611ac23d7f72cfe8448f6152e9222
GabHrf <gabrielh@live.fr>
2023-04-12 16:41:08 +0200

    Update easyadmin

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -219,4 +219,8 @@ class Client implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+    public function __toString(): string
+    {
+        return (string) $this->getEmail();
+    }
 }
