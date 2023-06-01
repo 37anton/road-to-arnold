918c8604b7bc6d03976db051bcb23b4a6c941300
harfouche <gabrielh@live.fr>
2023-06-01 14:33:03 +0200

    mini upd

--- src/Controller/SalleDeSportController.php
+++ src/Controller/SalleDeSportController.php
@@ -11,13 +11,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SalleDeSportController extends AbstractController
 {
-    #[Route('/salle-de-sport', name: 'app_salle_de_sport')]
+    #[Route('/salledesport', name: 'app_salle_de_sport')]
     public function index(SalleDeSportRepository $salledesportrepository): Response
     {
         $salledesports = $salledesportrepository->findAll();
 
         return $this->render('salle_de_sport/index.html.twig', [
             'salledesports' => $salledesports
+        ]);
+    }
+
+
+    #[Route('/salledesport', name: 'app_salle_de_sport_unit')]
+    public function show(SalleDeSportRepository $salledesportrepository): Response
+    {
+        $salledesport = $salledesportrepository->findAll();
+
+        return $this->render('salle_de_sport/show.html.twig', [
+            'salledesport' => $salledesport
         ]);
     }
 }
